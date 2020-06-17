f77f4c5c6ab6b39d7e180b5e7411ff1d02153e0f
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-06-17 15:13:00 +0300

    Initial Commit

---
+++ BlessYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlessYou
+//
+//  Created by Mor Soferian on 6/17/20.
+//  Copyright © 2020 Mor Soferian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
