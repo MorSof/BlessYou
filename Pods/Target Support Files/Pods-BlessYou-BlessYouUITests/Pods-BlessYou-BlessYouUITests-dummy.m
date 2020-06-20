9223a10170b16422fdd608cb585dd2d4dd7f4739
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-06-20 18:24:35 +0300

    * pods2

---
+++ Pods/Target Support Files/Pods-BlessYou-BlessYouUITests/Pods-BlessYou-BlessYouUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlessYou_BlessYouUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BlessYou_BlessYouUITests
+@end
---
+++ Pods/Target Support Files/Pods-BlessYou/Pods-BlessYou-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlessYou : NSObject
+@end
+@implementation PodsDummy_Pods_BlessYou
+@end
